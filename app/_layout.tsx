58169a3454d9f4a2f9e634e424fa565269c44e8f
fredzo <fborry@free.fr>
2025-10-07 16:57:06 +0200

    Added safe area.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Tabs } from 'expo-router';
 import { ExternalLink, MapPin, RadioTower, Settings } from 'lucide-react-native';
 import React from 'react';
 import { View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { FrameProvider } from '../providers/FrameProvider';
@@ -11,25 +12,31 @@ import { FrameProvider } from '../providers/FrameProvider';
 export default function Layout() {
   useKeepAwake();
   return (
-    <FrameProvider>
-      <AppContextProvider>
-        <View style={{flex:1}}>
-          <Header />
-          <Tabs screenOptions={{
-            headerShown:false,
-            tabBarShowLabel: false, // hide text
-            tabBarStyle: { backgroundColor: "#000" }, // dark background
-            tabBarActiveTintColor: "cyan",
-            tabBarInactiveTintColor: "gray",
-          }}>
-            <Tabs.Screen name="index" options={{ title: '', tabBarIcon: ({ color, size }) => <RadioTower color={color} size={size} /> }} />
-            <Tabs.Screen name="map" options={{ title: '', tabBarIcon: ({ color, size }) => <MapPin color={color} size={size} /> }} />
-            <Tabs.Screen name="data" options={{ title: '', tabBarIcon: ({ color, size }) => <ExternalLink color={color} size={size} /> }} />
-            <Tabs.Screen name="settings" options={{ title: '', tabBarIcon: ({ color, size }) => <Settings color={color} size={size} /> }} />
-          </Tabs>
-          <Footer />
-        </View>
-      </AppContextProvider>
-    </FrameProvider>
+    <SafeAreaProvider>
+      <FrameProvider>
+        <AppContextProvider>
+            <View style={{flex:1}}>
+              <SafeAreaView edges={['top']}>
+                <Header />
+              </SafeAreaView>
+              <Tabs screenOptions={{
+                headerShown:false,
+                tabBarShowLabel: false, // hide text
+                tabBarStyle: { backgroundColor: "#000" }, // dark background
+                tabBarActiveTintColor: "cyan",
+                tabBarInactiveTintColor: "gray",
+              }}>
+                <Tabs.Screen name="index" options={{ title: '', tabBarIcon: ({ color, size }) => <RadioTower color={color} size={size} /> }} />
+                <Tabs.Screen name="map" options={{ title: '', tabBarIcon: ({ color, size }) => <MapPin color={color} size={size} /> }} />
+                <Tabs.Screen name="data" options={{ title: '', tabBarIcon: ({ color, size }) => <ExternalLink color={color} size={size} /> }} />
+                <Tabs.Screen name="settings" options={{ title: '', tabBarIcon: ({ color, size }) => <Settings color={color} size={size} /> }} />
+              </Tabs>
+              <SafeAreaView edges={['bottom']}>
+                <Footer />
+              </SafeAreaView>
+            </View>
+        </AppContextProvider>
+      </FrameProvider>
+    </SafeAreaProvider>
   );
 }
